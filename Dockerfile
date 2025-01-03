1223b3ff99983b0a436ae43005f12e186e5a4edf
mako8231 <mm.neto2007@gmail.com>
2025-01-03 13:05:50 -0400

    Intervention and imagick installation

--- Dockerfile
+++ Dockerfile
@@ -25,8 +25,15 @@ RUN apt-get update && apt-get install -y \
     && docker-php-ext-configure gd --with-freetype --with-jpeg \
     && docker-php-ext-install -j$(nproc) gd
 
+
+
 # Install PHP extensions
 RUN docker-php-ext-install pdo pdo_mysql pdo_pgsql mbstring zip exif pcntl
+
+RUN apt-get update; \
+    apt-get install -y libmagickwand-dev; \
+    pecl install imagick; \
+    docker-php-ext-enable imagick
 
 # Clear cache
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
--- backend/app/Http/Controllers/PictureController.php
+++ backend/app/Http/Controllers/PictureController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Picture;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
+
 
 class PictureController extends Controller
 {
     public function index(Request $request, $id)
     {
-        return Storage::download(Picture::find($id)->file_url);
+        $manager = new ImageManager(new Driver());
+        $file_url = Picture::find($id)->file_url;
+        return $manager->read($file_url);
     }
 }
